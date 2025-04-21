34844a3c79607ff69d1d11a3dc85f5f4d4dd35d9
MadinaTadjidinova <madina.tadjidinova@gmail.com>
2025-04-21 18:18:14 +0600

    stoped render

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 API_TOKEN = '8044052060:AAFKriW7t-tUjkOJa19efEeg445be4OwDIE'
 ADMIN_USER_ID = 6946609744  # Telegram ID администратора
-ADMIN_USER_ID_MAIN = 893191426 
+ADMIN_USER_ID_MAIN = 893191426 #ok
 CHAT_ID = -1002363580068    # Для темы "Команда и Партнёры"
 THREAD_ID = 60              # ID темы "Команда и Партнёры"
